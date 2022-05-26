71869d1c97e97ee3582078e6006d73f4d46cdbac
spdy999 <1a1a1a.262626.0010@gmail.com>
2022-05-27 01:57:52 +0700

    feat(be): initial postgres database

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,18 @@ import * as bodyParser from "koa-bodyparser";
 
 const app = new Koa();
 const router = new Router();
+const { Pool } = require('pg');
 
-// Hello world
+// Get plain text
 router.get("/", async (ctx, next) => {
     ctx.body = {msg: "Hello world!!"}
     await next();
+});
+
+// Test db connection
+router.get('/pg', async (ctx) => {
+    const { rows } = await ctx.app.pool.query('SELECT $1::text as message', ['Hello, World!'])
+    ctx.body = rows[0].message;
 });
 
 interface HelloRequest {
@@ -34,8 +41,15 @@ app.use(bodyParser())
 // Routes
 app.use(router.routes()).use(router.allowedMethods());
 
-const port = process.env.SERVER_PORT || 4000
+app.pool = new Pool({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE_NAME,
+    password: process.env.DB_PASSWORD, // Password is empty be default
+    port: process.env.POSTGRES_PORT, // Default port
+});
 
+const port = process.env.SERVER_PORT || 4000
 app.listen(port, () => {
     console.log("Koa started");
 });
